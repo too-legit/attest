43dbc0a87f3d67d34a51c5fab525d8a9bb834b1b
Brian DeHamer <bdehamer@github.com>
2024-09-03 15:21:38 -0700

    debugging
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

---
+++ index.js
@@ -0,0 +1,16 @@
+import attest from '@actions/attest'
+import {bundleToJSON} from '@sigstore/bundle'
+
+const main = async () => {
+  const attestation = await attest.attestProvenance({
+    subjectName: 'bin-linux.tgz',
+    subjectDigest: { 'sha256': '40d117f04fa3970c2c852d2c6e0f5a9876fa8eb1c2e6ee6abe58bef58a7aa93a' },
+    token: process.env.GITHUB_TOKEN,
+    issuer: 'https://token.actions.githubusercontent.com/hammer-time',
+    skipWrite: true
+  });
+
+  console.log(JSON.stringify(attestation));
+}
+
+await main()
