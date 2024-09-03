a5da9866508655ffc466288b285f8c0b0bed26fc
Brian DeHamer <bdehamer@github.com>
2024-09-03 15:38:40 -0700

    more debug
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const main = async () => {
     subjectDigest: { 'sha256': '40d117f04fa3970c2c852d2c6e0f5a9876fa8eb1c2e6ee6abe58bef58a7aa93a' },
     token: process.env.GITHUB_TOKEN,
     issuer: 'https://token.actions.githubusercontent.com/hammer-time',
-    skipWrite: true
   });
 
   console.log(JSON.stringify(attestation));
