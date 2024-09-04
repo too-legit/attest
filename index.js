83a6a3c9312a501e67e9364185bb6a089c16aa31
Brian DeHamer <bdehamer@github.com>
2024-09-04 07:56:55 -0700

    skipWrite: false
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const main = async () => {
     subjectName: 'bin-linux.tgz',
     subjectDigest: { 'sha256': 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' },
     token: process.env.GITHUB_TOKEN,
-    issuer: 'https://token.actions.githubusercontent.com/hammer-time',
-    skipWrite: true
+    issuer: 'https://token.actions.githubusercontent.com/hammer-time'
   });
 
   console.log(JSON.stringify(attestation));
